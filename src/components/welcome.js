ec0910ae8858df0e2b4606b8f3c396d8c8c2d3a5
Ben Scott <bengscott2@gmail.com>
2019-09-17 10:03:00 +0100

    chohre: add welcome hook.

---
+++ src/components/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './welcome.css'
+
+function Welcome() {
+
+  return (
+    <div className='welcome-header'>
+      Welcome.
+    </div>
+  );
+}
+
+export default Welcome
