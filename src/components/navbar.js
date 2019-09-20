d9d1be3248eca3326e60113d53cf346c70bab1b6
Ben Scott <bengscott2@gmail.com>
2019-09-20 14:56:05 +0100

    style: add border to toolbars

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,29 +2,32 @@ import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import './MuiToolbar.css'
 
 function Navbar() {
 
   return (
     <React.Fragment>
-        <Typography
-          component="h2"
-          variant="h5"
-          color="inherit"
-          align="center"
-          noWrap
-          className='name-bar'
-        >
-          Benjamin Scott
-        </Typography>
-        <Toolbar className='navbar'>
+        <Toolbar>
+          <Typography
+            component="h2"
+            variant="h5"
+            color="inherit"
+            align="center"
+            noWrap
+            className='name-bar'
+          >
+            Benjamin Scott
+          </Typography>
+        </Toolbar>
+        <Toolbar>
           <Button size='small'>
             About Me
           </Button>
           <Button
             size='small'
             align='center'
-            >
+          >
             Projects
           </Button>
           <Button size='small'>
