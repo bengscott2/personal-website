a524cdc2a838d5013377d2842466dfe7d1d2c98d
Ben Scott <bengscott2@gmail.com>
2019-09-23 10:17:50 +0100

    style: add borrder and fonts to navbar and namebar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import './MuiToolbar.css'
+import './navbar.css'
 
 function Navbar() {
 
   return (
     <div>
-      <h1 className='name-bar'>BENJAMIN SCOTT</h1>
-      <ul>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>CV</li>
-        <li  style={{float:'right'}}>Contact</li>
-      </ul>
+      <h1 className='name-bar'>Benjamin Scott</h1>
+      <div>
+        <ul className='nav-bar'>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>CV</li>
+          <li  style={{float:'right'}}>Contact</li>
+        </ul>
+      </div>
     </div>
   )
 }
